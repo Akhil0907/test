268a685062f1289b383d2b623503a81c8ebc48a3
Akhil0907 <45623246+Akhil0907@users.noreply.github.com>
2023-03-30 14:31:05 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,7 +4,7 @@ const { test, expect } = require('@playwright/test');
 test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
+  // Expect a title "to contain" a substring
   await expect(page).toHaveTitle(/Playwright/);
 
   // create a locator
